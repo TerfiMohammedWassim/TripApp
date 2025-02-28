6c64065bf1c037cc16941333170b29765e82c41e
PredatorXxRIPxX <wassimou009@gmail.com>
2025-03-01 00:29:04 +0100

    fixing

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/TripApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/TripApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TripApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2ae57d256521d41a420afee7ecb20d9bba20548c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4e236c34c969f30e73d18a3873ae35655f2e69ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TripApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TripApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
